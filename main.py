bee918f71f80045b10f0b53bb4036cfc4eae7d9c
Dein Name <du@example.com>
2022-06-08 21:02:14 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# ' Only Hello World'
+print("Hello Wolrd")
+
+# Hello and the given Name
+def hello_world(name):
+    print("Hello",name)
+    
+
+hello_world("Hasanain")
+hello_world("Bangaly")
